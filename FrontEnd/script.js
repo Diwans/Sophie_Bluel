042e01342594a4eccac46042044b32002dbb165a
Diwan Schneider <diwanschneide@yahoo.fr>
2023-02-14 20:55:14 +0100

    fetch

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,13 +1,21 @@
 const gallery = document.querySelector('.gallery')
+const img = document.querySelectorAll('img')
+console.log(img)
 
 
 
 
+fetch(`http://localhost:5678/api/works`)
+    .then(res => {
+        console.log(res);
 
-async function apiData(){
-    fetch(`http://localhost:5678/api/works`)
-        .then(res => res.json())
-        .then(data => console.log(data))
-}
+        if(res.ok){
+            res.json().then(data => {
+                gallery.innerHTML +="<figure>"+"<img>"+"<figcaption>"+data[0].title+"</figcaption>"+"</figure>";
+                img.src = data[0].imageUrl
+            })
+        } else {
+            console.log("ERREUR");
+        }
+    })
 
-apiData()
