ea6d97c53d2b34a70926de677d6cafb86c01ea9c
Diwan Schneider <diwanschneide@yahoo.fr>
2023-02-25 17:52:01 +0100

    test pour filtrer les travaux

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,24 +1,27 @@
 const gallery = document.querySelector('.gallery')
-const img = document.querySelectorAll('.imgGallery')
 const cateContainer = document.getElementById('categories')
 
 
-
-fetch(`http://localhost:5678/api/works`)
+function galleryFetch(){
+    fetch(`http://localhost:5678/api/works`)
     .then(res => {
         console.log(res);
 
         if(res.ok){
             res.json().then(data => {
                 for ( let i=0; i < data.length; i++){
-                gallery.innerHTML +="<figure data-attr-categorie="+data[i].category.id+">"+"<img src="+data[i].imageUrl+">"+"<figcaption>"+data[i].title+"</figcaption>"+"</figure>";
-            }
+                gallery.innerHTML +="<figure data-attr-categorie="+data[i].category.id+" class=\"imgGallery\">"+"<img src="+data[i].imageUrl+">"+"<figcaption>"+data[i].title+"</figcaption>"+"</figure>";
+                }
+                getImg()
             })
         } else {
             console.log("ERREUR");
         }
     });
+}
+galleryFetch()
 
+function buttonFetch(){
     fetch(`http://localhost:5678/api/categories`)
     .then(res => {
         console.log(res);
@@ -26,24 +29,48 @@ fetch(`http://localhost:5678/api/works`)
         if(res.ok){
             res.json().then(data => {
                 for ( let i=0; i < data.length; i++){
-                cateContainer.innerHTML +="<button data-attr-categorie="+data[i].id+" class=\"textCategories\">"+data[i].name+"</button>"
+                cateContainer.innerHTML +="<button data-attr-categorie="+data[i].id+" class=\"textCategory\">"+data[i].name+"</button>"
             }
-            eventButton()
+            getIdBtn()
 
             })
         } else {
             console.log("ERREUR");
         }
     });
+}
+buttonFetch()
 
 
-function eventButton (){
-    const btnCategory = document.querySelectorAll('.textCategories')
-    for ( let i= 0 ; i< btnCategory.length; i++)
+function getIdBtn(){
+    const btnCategory = document.querySelectorAll('.textCategory')
+    for ( let i=0 ; i < btnCategory.length; i++)
     btnCategory[i].addEventListener('click', (e) => {
-        console.log(e.target.dataset.attrCategorie)
-    } )    
+        let value = e.target.dataset.attrCategorie
+        console.log(value)
+        
+        if (value == 0){
+            console.log('a')
+        }
+
+        if (value == 1){
+            gallery.innerHTML="";
+           
+        }
+
+        if (value == 2){
+            console.log('c')
+        }
+
+        if (value == 3){
+            console.log('d')
+        }
+        
+    } ) 
 }
 
-
+function getImg(){
+    const img = document.querySelectorAll('.imgGallery')
+    console.log(img)
+}
 
